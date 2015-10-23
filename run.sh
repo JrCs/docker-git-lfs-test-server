0a8e5648c14f3abb7294dbf5183d30e577979d17
Yves Blusseau <90z7oey02@sneakemail.com>
2015-10-23 10:09:10 +0200

    Update certificates management

--- run.sh
+++ run.sh
@@ -4,9 +4,8 @@ set -eu
 set -o pipefail
 
 CERTS_DIR=/data/certs
-[ -f /data/certs/server.crt ] && export LFS_CERT=/data/certs/server.crt
-[ -f /data/certs/server.key ] && export LFS_KEY=/data/certs/server.key
-
-[ -n "$LFS_CERT" -a -n "$LFS_KEY" ] && export LFS_SCHEME="https"
+[ ! -d "$CERTS_DIR" ] && mkdir -p $CERTS_DIR
+[ ! "${LFS_CERT:0:1}" == / ] && export LFS_CERT="$CERTS_DIR/$LFS_CERT"
+[ ! "${LFS_KEY:0:1}"  == / ] && export LFS_KEY="$CERTS_DIR/$LFS_KEY"
 
 exec /bin/lfs-test-server
